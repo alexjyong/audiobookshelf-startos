2eebd013f232e3d7e458dac5fa8bf4233b646e58
Alex Yong <alexjyong@gmail.com>
2025-04-07 19:02:41 +0000

    stashing changes

--- codespace-scripts/postCreateScript.sh
+++ codespace-scripts/postCreateScript.sh
@@ -3,9 +3,14 @@
 docker buildx install 
 docker buildx create --use
 docker run --privileged --rm linuxkit/binfmt:v0.8
-git clone https://github.com/Start9Labs/start-os.git
-cd start-os
-git submodule update --init --recursive 
-make sdk 
+# git clone https://github.com/Start9Labs/start-os.git
+# cd start-os
+# git submodule update --init --recursive 
+# make sdk 
+wget https://github.com/alexjyong/audiobookshelf-startos/releases/download/start-cli-scripts/start-sdk-utilities.tar
+tar -xf https://github.com/alexjyong/audiobookshelf-startos/releases/download/start-cli-scripts/start-sdk-utilities.tar
+cd start-sdk-utilities
+mv start-* /usr/local/cargo/bin/
+
 start-sdk init 
 start-sdk --version
